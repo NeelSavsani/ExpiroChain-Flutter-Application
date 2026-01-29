c97c645375deed5c897f30449f8fc03a23ba8710
NeelSavsani <neelsavsani7@gmail.com>
2026-01-29 23:33:44 +0530

    solved error in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:expirochain_app/screens/home_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:expirochain_app/main.dart';
@@ -13,7 +14,12 @@ import 'package:expirochain_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: HomeScreen(firmName: "Test Firm"),
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
